ebeca7c29a53e0dd8e7052a11b4a8a2fc0e8d678
NabilMouami <86678449+NabilMouami@users.noreply.github.com>
2022-05-08 16:40:42 +0100

    Add files via upload

---
+++ backend/models/blogModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  name: String,
+  creator: String,
+  tags: [String],
+  imageFile: String,
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  likes: {
+    type: [String],
+    default: [],
+  },
+});
+
+const BlogModal = mongoose.model("Blog", blogSchema);
+
+module.exports = BlogModal;
